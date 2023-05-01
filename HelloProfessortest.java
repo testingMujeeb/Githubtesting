2caa6330b4c5b5dce10b5afa6790802e14c0b8c3
testingMujeeb <132301273+testingMujeeb@users.noreply.github.com>
2023-05-01 16:05:12 -0400

    Modified hello professors test
    additon to gitbub

--- HelloProfessortest.java
+++ HelloProfessortest.java
@@ -1,5 +1,6 @@
 public class HelloProfessortest{
         public static void main(String []args){
          System.out.println("Hello Professor");
+         System.out.println("GITHUB IS THE BEST");
         }
 }
